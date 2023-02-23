7453ac365ce09fa58e6f6aee284dba664018a6ec
TripleNine666 <kolumb1709@gmail.com>
2023-02-23 15:03:20 +0300

    Not Found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,11 @@
 <template>
-  <section>Error</section>
+  <section>
+    <base-card>
+      <h2>Page Not Founded</h2>
+      <p>
+        Page not founded - may be you want to find some
+        <router-link to="/coaches">coach</router-link> for you?
+      </p>
+    </base-card>
+  </section>
 </template>
